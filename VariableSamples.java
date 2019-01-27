d132bc0cac9ca57149a6612177c79584bf2948a9
marksg47 <msgoroy@gmail.com>
2019-01-27 15:23:57 +0800

    final revisions

--- VariableSamples.java
+++ VariableSamples.java
@@ -16,18 +16,23 @@ public class VariableSamples
 		//grade, double data type and initialized
 		//to 0.0
 
-		double grade =0.0;
+		double grade = 0.0;
 
-		System.out.println("Result: " + result);
-		System.out.println("Option: " + option);
-		System.out.println("Grade: " + grade);
+		int counter = 5;
+
+		//System.out.println("Result: " + result);
+		//System.out.println("Option: " + option);
+		//System.out.println("Grade: " + grade);
+		System.out.println("Counter: " + counter);
 
 		result = false;
 		option = 'G';
 		grade = 1.0;
+		counter++;
 
-		System.out.println("Result: " + result);
-		System.out.println("Option: " + option);
-		System.out.println("Grade: " + grade);
+		//System.out.println("Result: " + result);
+		//System.out.println("Option: " + option);
+		//System.out.println("Grade: " + grade);
+		System.out.println("Counter: " + counter);
 	}
 }
