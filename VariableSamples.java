0192cafea4aeedfd245dcfd5d8c9eeced0350e91
marksg47 <msgoroy@gmail.com>
2019-01-27 14:39:32 +0800

    first Variable Samples code

---
+++ VariableSamples.java
@@ -0,0 +1,21 @@
+public class VariableSamples
+{
+	public static void main(String[] args)
+	{
+		//declare a data type with variable name
+		//result and boolean data type
+		boolean result;
+
+		//declare a data type with variable name
+		//option and char data type
+		char option;
+		option = 'C';
+		//assign 'C' to option
+
+		//declare a data type with variable name
+		//grade, double data type and initialized
+		//to 0.0
+
+		double grade =0.0;
+	}
+}
